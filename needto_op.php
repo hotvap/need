d74bd70bbf4241d925864493d604fdd6e1ae14aa
parad0x <onestyle@list.ru>
2016-10-25 11:01:58 +0300

    test

--- needto_op.php
+++ needto_op.php
@@ -1692,6 +1692,8 @@ if( isset( $_REQUEST['op'] ) and is_numeric($_REQUEST['op']) ){
                                 $body=$body->fetchAssoc();
                                 if( isset($body['field_youtube_video_id']) and strlen($body['field_youtube_video_id']) ){
                                     $outs['apages'][$isnid2]['video']=$body['field_youtube_video_id'];
+                                }
+                                if( isset($body['body_value']) and strlen($body['body_value']) ){
                                     $outs['apages'][$isnid2]['body']=$body['body_value'];
                                 }
                             }
